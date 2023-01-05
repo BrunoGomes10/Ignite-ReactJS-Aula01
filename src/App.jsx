b9704eff290ff6b63eeb0f8b855c442ad9e87e27
BrunoGomes10 <brunogomes.gomes@gmail.com>
2023-01-05 17:28:34 -0300

    My Second Commit All Files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <Post
+      author="Bruno Gomes"
+      content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ullam molestiae similique sed eos reiciendis laboriosam repellat accusantium harum veniam in consequatur doloribus, commodi qui, quis voluptatum, nemo fugit inventore."
+    />
+  )
+}
+
