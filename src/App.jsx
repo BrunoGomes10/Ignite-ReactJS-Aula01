2676fa0b5d3bef721da44f6ecb521922098fca41
BrunoGomes10 <brunogomes.gomes@gmail.com>
2023-01-08 22:14:46 -0300

    ReactJS - Estruturando/Estilizando Header/SideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 
-import { Post } from "./Post"
+import { Header } from './components/Header';
+import { Post } from './Post';
+
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
-    <Post
-      author="Bruno Gomes"
-      content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ullam molestiae similique sed eos reiciendis laboriosam repellat accusantium harum veniam in consequatur doloribus, commodi qui, quis voluptatum, nemo fugit inventore."
-    />
-  )
+      <div>
+        <Header />
+
+        <div className={styles.wrapper}>
+          <Sidebar />
+          <main>
+            <Post
+              author="Bruno Gomes"
+              content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ullam molestiae similique sed eos reiciendis laboriosam repellat accusantium harum veniam in consequatur doloribus, commodi qui, quis voluptatum, nemo fugit inventore."
+            />
+
+            <Post
+              author="Will"
+              content="Outro post muito legal !!!"
+            />
+          </main>
+        </div>
+
+      </div>
+  );
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50" alt="" />
+
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/brunogomes10.png" alt="" />
+
+                <strong>Bruno Gomes</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+
+        </aside>
+
+        
+
+    );
+}
